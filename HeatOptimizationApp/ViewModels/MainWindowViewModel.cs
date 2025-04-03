b12efadcf266403208ecbd91664a1cc9a0f670d2
rafacedric <rafelcedric@gmail.com>
2025-04-03 16:07:44 +0200

    alittlebitmoreavalonia

--- HeatOptimizationApp/ViewModels/MainWindowViewModel.cs
+++ HeatOptimizationApp/ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,5 @@
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string Greeting { get; } = "This is our semester project! It completly doesnt work but that is what we have :)";
 }
