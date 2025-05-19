f87174b2b140ec4fa392f9716381e6c0bf8f8ec8
Simonas Ju <ae.62rq4@aleeas.com>
2025-05-19 14:19:13 +0300

    stacked chart doesn't work

--- HeatOptimizationApp/Views/OptimizerWindow.axaml.cs
+++ HeatOptimizationApp/Views/OptimizerWindow.axaml.cs
@@ -1,11 +1,30 @@
 using Avalonia.Controls;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Collections.Generic;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView.Painting;
+using HeatOptimizationApp.Models;
+using System.Linq;
+using LiveChartsCore.Measure;
 
 namespace HeatOptimizationApp.Views;
 
 public partial class OptimizerWindow : Window
 {
+    public ISeries[] Series { get; set; }
+
     public OptimizerWindow()
     {
+        Series = new ISeries[]
+        {
+            new StackedAreaSeries<double>(new double[] { 3, 2, 3, 5, 3, 4, 6 }),
+            new StackedAreaSeries<double>(new double[] { 6, 5, 6, 3, 8, 5, 2 }),
+            new StackedAreaSeries<double>(new double[] { 4, 8, 2, 8, 9, 5, 3 })
+        };
+
         InitializeComponent();
+        DataContext = this;
+        WinterChart.Series = Series;
     }
 }
--- HeatOptimizationApp/obj/Debug/net9.0/linux-x64/HeatOptimizationApp.AssemblyInfo.cs
+++ HeatOptimizationApp/obj/Debug/net9.0/linux-x64/HeatOptimizationApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f20306c6bff87a85776494bedcb54ed6ec222e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a36e5fe50d3c333f49d22d1bf0bcd1086adc0b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
