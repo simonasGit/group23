6496b5c44e0100c8351062e00f91dda07e14ba1a
rafacedric <rafelcedric@gmail.com>
2025-05-29 11:51:46 +0200

    Better small changes to the UI

--- HeatOptimizationApp/obj/Debug/net9.0/HeatOptimizationApp.AssemblyInfo.cs
+++ HeatOptimizationApp/obj/Debug/net9.0/HeatOptimizationApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5304b7f1e8e7e334a080c80d042e6b67f8730c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c6e934c2872239dd1a34a7f0d48d0a12788514")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
