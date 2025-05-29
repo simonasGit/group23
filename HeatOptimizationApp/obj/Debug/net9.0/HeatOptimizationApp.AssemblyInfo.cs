82c6e934c2872239dd1a34a7f0d48d0a12788514
rafacedric <rafelcedric@gmail.com>
2025-05-29 11:35:06 +0200

    MainWindow Icons

--- HeatOptimizationApp/obj/Debug/net9.0/HeatOptimizationApp.AssemblyInfo.cs
+++ HeatOptimizationApp/obj/Debug/net9.0/HeatOptimizationApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472a5bd63379e0c76a1b3270cf9bb613e0fafc9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5304b7f1e8e7e334a080c80d042e6b67f8730c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatOptimizationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
